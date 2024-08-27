1889e385535de7dc67fab273fa1d6252aef3eaca
Tanwie12 <adeytanwie@gmail.com>
2024-08-27 18:35:05 +0400

    added logo

--- components/layouts/Topbar.tsx
+++ components/layouts/Topbar.tsx
@@ -15,7 +15,7 @@ const [isOpen, setIsOpen] = React.useState(false)
 const pathName=usePathname()
   return (
     <div className='sticky top-0 z-20 w-full flex justify-between items-center px-8 py-4 b-blue-2 shadow-xl lg:hidden'>
-         <Image className='w-10 h-10' src='/logo.svg' alt='logo' width={100} height={100}/>
+         <Image className='w-10 h-10' src='/logo.png' alt='logo' width={100} height={100}/>
         <div className='flex max-md:hidden  gap-4'>
             {   navLinks.map(link => (
                 <Link href={link.url} key={link.label} className={`flex flex-row text-body-medium items-center gap-4 ${pathName===link.url ? "text-blue-1" : "" }`}>
